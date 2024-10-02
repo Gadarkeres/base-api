67c28bd853f0ec75d7ef03bf746f8313419e4579
Matheus Rafael <matheusrafael77@hotmail.com>
2024-10-02 10:35:15 -0300

    create data-base-h2

--- src/test/java/com/exampl/api/ApiController.java
+++ src/test/java/com/exampl/api/ApiController.java
@@ -1,15 +1,23 @@
 package com.exampl.api;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 
   ModelMapper mapper = new ModelMapper();
+
+  @Autowired
+  private UserRepository repository;
 
   @GetMapping("hello-world")
   public String Hello() {
@@ -26,5 +34,15 @@ public class ApiController {
     RequestResponseDTO requestResponseDTO = mapper.map(RequestDTO.class, RequestResponseDTO.class);
 
     return requestResponseDTO;
+  }
+
+  @GetMapping("/users")
+  public List<User> getUsers() {
+    return repository.findAll();
+  }
+
+  @PostMapping("/users/insert")
+  public void insertUser(@RequestBody User user) {
+    repository.save(user);
   }
 }
